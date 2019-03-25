bd7e473d390c06b4e41e3d06882d80abab802084
Pany <1246693651@qq.com>
2019-03-25 14:20:51 +0800

    基本结构和首页基本设计

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -30,6 +30,15 @@ App({
             }
           })
         }
+      }
+    })
+    // 获取系统状态栏信息
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
       }
     })
   },
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,130 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+const app = getApp();
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 12,
+      name: 'VR'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '录像'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '图像'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '通知'
+    }, {
+      icon: 'upstagefill',
+      color: 'cyan',
+      badge: 0,
+      name: '排行榜'
+    }, {
+      icon: 'clothesfill',
+      color: 'blue',
+      badge: 0,
+      name: '皮肤'
+    }, {
+      icon: 'discoverfill',
+      color: 'purple',
+      badge: 0,
+      name: '发现'
+    }, {
+      icon: 'questionfill',
+      color: 'mauve',
+      badge: 0,
+      name: '帮助'
+    }, {
+      icon: 'commandfill',
+      color: 'purple',
+      badge: 0,
+      name: '问答'
+    }, {
+      icon: 'brandfill',
+      color: 'mauve',
+      badge: 0,
+      name: '版权'
+    }],
+    gridCol: 3,
+    skin: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  gridchange: function (e) {
+    this.setData({
+      gridCol: e.detail.value
+    });
+  },
+  gridswitch: function (e) {
+    this.setData({
+      gridBorder: e.detail.value
+    });
+  },
+  menuBorder: function (e) {
+    this.setData({
+      menuBorder: e.detail.value
+    });
+  },
+  menuArrow: function (e) {
+    this.setData({
+      menuArrow: e.detail.value
+    });
+  },
+  menuCard: function (e) {
+    this.setData({
+      menuCard: e.detail.value
+    });
+  },
+  switchSex: function (e) {
+    this.setData({
+      skin: e.detail.value
+    });
+  },
+
+  // ListTouch触摸开始
+  ListTouchStart(e) {
+    this.setData({
+      ListTouchStart: e.touches[0].pageX
+    })
+  },
+
+  // ListTouch计算方向
+  ListTouchMove(e) {
+    this.setData({
+      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // ListTouch计算滚动
+  ListTouchEnd(e) {
+    if (this.data.ListTouchDirection == 'left') {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        modalName: e.currentTarget.dataset.target
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      this.setData({
+        modalName: null
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      ListTouchDirection: null
     })
-  }
+  },
 })
