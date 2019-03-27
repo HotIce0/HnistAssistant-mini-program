430640ebb668ca6d9c7ff0c5d850602c27c4115e
Pany <1246693651@qq.com>
2019-03-27 22:39:58 +0800

    二手市场模板

--- pages/secondhand/index.js
+++ pages/secondhand/index.js
@@ -4,6 +4,23 @@ Page({
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     Custom: app.globalData.Custom,
+    TabCur: 0,
+    TabCur1: 0,
+    scrollLeft: 0,
+    sort:['最新','最热']
   },
-
+  tabSelect(e) {
+    console.log(e);
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  },
+  tabSelect1(e) {
+    console.log(e);
+    this.setData({
+      TabCur1: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  }
 })
